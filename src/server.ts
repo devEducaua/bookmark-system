f0da141cb32307a3c2e05d5d34c1756d7db3a7de
devEducaua <schokito4@gmail.com>
2026-01-17 09:25:16 -0300

    feat: add 404 error

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,21 @@ import { Context, Hono } from "hono";
 import { jwt } from "hono/jwt";
 import { userController } from "./controllers/userController";
 import { bookmarkController } from "./controllers/bookmarkController";
-import { DomainError, AuthError } from "./utils/errors";
+import { DomainError, AuthError, NotFoundError } from "./utils/errors";
 
 const app = new Hono().basePath("/api");
 
 app.onError((err: Error, c: Context) => {
     if (err instanceof DomainError)
-        return c.json(err.message, 400);
+        return c.json({ err: err.message }, 400);
 
     if (err instanceof AuthError)
-        return c.json(err.message, 401);
+        return c.json({ err: err.message }, 401);
 
-    return c.json("internal server error", 500);
+    if (err instanceof NotFoundError)
+        return c.json({ err: err.message }, 404);
+
+    return c.json({ err: "internal server error" }, 500);
 
 })
 
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,4 +1,4 @@
 
 export class DomainError extends Error {}
-
 export class AuthError extends Error {}
+export class NotFoundError extends Error {}
